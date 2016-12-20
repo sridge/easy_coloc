1142bb6a7a5e25932af4b4e87912543e6c4bee1c
Raphael Dussin <raphael.dussin@gmail.com>
2016-12-20 15:40:24 -0500

    minor fix

--- examples/roms_to_seawifs.py
+++ examples/roms_to_seawifs.py
@@ -6,7 +6,7 @@ roms_data = '/Users/raphael/STORAGE/ROMS/CCS1-RD.NVOcobalt22S_avg_1995-01-17T00:
 seawifs_clim = '../data/seawifs_december.nc'
 
 roms2seawifs = easy_coloc.easy_coloc(roms_grid,from_global=False,coord_names=['lon_rho','lat_rho'],mask_var='mask_rho')
-lon_obs, lat_obs, data_obs, jindex, iindex = roms2seawifs.define_obs_position_from_gridded(seawifs_clim,'chl',frame=11)
+lon_obs, lat_obs, data_obs, jindex, iindex = roms2seawifs.define_obs_position_from_gridded(seawifs_clim,'chl',frame=0)
 out = roms2seawifs.interpolate_model_onto_obs_space(lon_obs,lat_obs,roms_data,'temp',level=-1,frame=0,spval=1.0e+15)
 out_2d = roms2seawifs.reshape_interpolated_data(jindex,iindex,out)
 

b3c91fbe23f6354cb7da8d0e9da5268233aa6f16
Raphael Dussin <raphael.dussin@gmail.com>
2017-01-24 18:52:34 -0500

    more explanations

--- examples/roms_to_copedpod.py
+++ examples/roms_to_copedpod.py
@@ -5,12 +5,18 @@ roms_grid = '/Users/raphael/STORAGE/ROMS/GRIDS/CCS_7k_0-360_fred_grd.nc'
 roms_data = '/Users/raphael/STORAGE/ROMS/CCS1-RD.NVOcobalt22S_avg_1995-01-17T00:00:00.nc'
 copepod_JJA = '/Users/raphael/TOOLS/workspace-raphael.git/data_processing/copepod_nvo/copepod-2012_cmass_20160729.csv'
 
+# create easy_coloc object
 roms2copepod     = easy_coloc.easy_coloc(roms_grid,from_global=False,coord_names=['lon_rho','lat_rho'],mask_var='mask_rho')
+#  define list of obs positions from text file
 lon_obs, lat_obs = roms2copepod.define_obs_position_from_text(copepod_JJA,0,1,offset_line=1,separator=',',if_found='JJA')
+# project model data onto observation space
 out              = roms2copepod.interpolate_model_onto_obs_space(lon_obs,lat_obs,roms_data,'temp',level=-1,frame=0,spval=1.0e+15)
 
-plt.figure()
-plt.plot(lon_obs,lat_obs,'ko')
-#plt.pcolor(roms2seawifs.lon_gridded_2d,roms2seawifs.lat_gridded_2d,out_2d); plt.colorbar()
-#plt.clim([8.,18.])
-plt.show()
+# write model data projected onto obs space into text file
+roms2copepod.write_to_file('roms_onto_copepod.txt',lon_obs,lat_obs,out)
+
+#----------plotting positions ---------------------
+#plt.figure()
+#plt.plot(lon_obs,lat_obs,'ko')
+#plt.show()
+
--- examples/roms_to_seawifs.py
+++ examples/roms_to_seawifs.py
@@ -5,16 +5,24 @@ roms_grid = '/Users/raphael/STORAGE/ROMS/GRIDS/CCS_7k_0-360_fred_grd.nc'
 roms_data = '/Users/raphael/STORAGE/ROMS/CCS1-RD.NVOcobalt22S_avg_1995-01-17T00:00:00.nc'
 seawifs_clim = '../data/seawifs_december.nc'
 
+# create an easy_coloc object, basically defining model grid
 roms2seawifs = easy_coloc.easy_coloc(roms_grid,from_global=False,coord_names=['lon_rho','lat_rho'],mask_var='mask_rho')
+# from seawifs netcdf file, we extract the position of the valid observation (non-masked or non-missing value)
+# create a list of positions in geographical and obs grid space
 lon_obs, lat_obs, data_obs, jindex, iindex = roms2seawifs.define_obs_position_from_gridded(seawifs_clim,'chl',frame=0)
+# project model data onto observation space
 data_model = roms2seawifs.interpolate_model_onto_obs_space(lon_obs,lat_obs,roms_data,'temp',level=-1,frame=0,spval=1.0e+15)
-out_2d = roms2seawifs.reshape_interpolated_data(jindex,iindex,data_model)
 
+# write obs data into text file that can be read by pandas
 roms2seawifs.write_to_file('obs.txt',lon_obs,lat_obs,data_obs)
+# write model data into text file that can be read by pandas
 roms2seawifs.write_to_file('model.txt',lon_obs,lat_obs,data_model)
 
+#------- plot the results -----------
+
+# reshape model data projected onto obs space into plotable 2d array
+#out_2d = roms2seawifs.reshape_interpolated_data(jindex,iindex,data_model)
 
 #plt.figure()
-##plt.plot(lon_obs,lat_obs,'ko')
 #plt.pcolor(roms2seawifs.lon_gridded_2d,roms2seawifs.lat_gridded_2d,out_2d); plt.colorbar()
 #plt.show()

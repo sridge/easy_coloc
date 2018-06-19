757e52e9858e6484b07d6faef52a6977137f9331
Raphael Dussin <raphael.dussin@gmail.com>
2018-06-19 12:11:08 -0400

    change html theme

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Raphael Dussin'
 author = 'Raphael Dussin'
 
 # The short X.Y version
-version = ''
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -75,7 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

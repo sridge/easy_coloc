fix masking issue + example llc90

easy_coloc/lib_easy_coloc.py
@@ -61,11 +61,9 @@ class projection():
         level : int
 
         '''
-        # not sure this is doing what I'm thinking
-        if mask_value is not None:
-            self.mask_values = _np.array([mask_value])
-        else:
-            self.mask_values = _np.array([0])
+        # TO DO: ideally we'd like to use the src_mask_values property
+        # of the ESMF Regridder. But that requires creating an object for each level
+        # It seems that setting the masked points to np.nan does the work at lower cost
 
         # create field object for model data
         field_model = _ESMF.Field(self.model_grid, staggerloc=_ESMF.StaggerLoc.CENTER)
@@ -74,8 +72,9 @@ class projection():
 
         interpolator = _ESMF.Regrid(field_model, field_obs,
                                     regrid_method=_ESMF.RegridMethod.BILINEAR,
-                                    unmapped_action=_ESMF.UnmappedAction.IGNORE,
-                                    src_mask_values = self.mask_values) # esmf uses mask value, not spval
+                                    unmapped_action=_ESMF.UnmappedAction.IGNORE)#,
+                                    #src_mask_values = self.mask_values) # esmf uses mask value, not spval
+
         if type(data) != 'numpy.array':
             data = data.values
 
@@ -86,7 +85,12 @@ class projection():
 
         for kframe in _np.arange(nframes):
             for klevel in _np.arange(nlevels):
-                field_model.data[:] = data[kframe,klevel,:,:].transpose()
+                datain = data[kframe,klevel,:,:].transpose()
+                if mask_value is not None:
+                    datain[_np.where(datain == mask_value)] = _np.nan # ugly, cf above
+
+                field_model.data[:] = datain
+
                 # run the interpolator
                 field_obs = interpolator(field_model, field_obs)
                 data_model_interp = field_obs.data.copy()

examples/llc90_temp_onto_AR07W.py
@@ -4,21 +4,26 @@ import pandas as pd
 import cartopy as cart
 import matplotlib.pylab as plt
 from matplotlib import cm
+import xmitgcm
+import numpy as np
 
 # load stations information from csv file
 ar07w = pd.read_csv('../data/AR07W_stations.txt',skipinitialspace=True)
 
 # load gridded dataset
-ds = xr.open_dataset('../data/woa_labrador.nc',decode_times=False)
+ds = xmitgcm.open_mdsdataset('../data/global_oce_llc90/',prefix=['T'],geometry='llc')
+
+# quick look at the input data, the face we need for AR07W is #10
+#ds['T'].sel(face=10,k=0,time=8).plot(cmap=cm.gist_ncar); plt.show()
 
 # create source grid and target section objects
 # this requires lon,lat from stations and the source grid dataset containing lon,lat
-proj = lib_easy_coloc.projection(ar07w['lon'].values,ar07w['lat'].values,grid=ds,
-                                 from_global=False)
+# here subsetting face 10 of ds and passing the names of lon/lat coords in ds
+proj = lib_easy_coloc.projection(ar07w['lon'].values,ar07w['lat'].values,grid=ds.sel(face=10),
+                                 coord_names=['XC','YC'],from_global=False)
 
 # run the projection on the WOA analyzed temperature (t_an)
-fld = proj.run(ds['t_an'][:])
-
+fld = proj.run(ds['T'].sel(face=10),mask_value=0)
 
 plt.figure(figsize=[6,6])
 m = plt.axes(projection=cart.crs.PlateCarree())
@@ -29,7 +34,7 @@ m.set_extent([-75, -35, 35, 65], crs=cart.crs.PlateCarree())
 gl = m.gridlines(draw_labels=True)
 
 plt.figure(figsize=[6,6])
-plt.contourf(ar07w['lat'].values,-ds['depth'],fld[0,:,:],30,cmap=cm.gist_ncar)
+plt.contourf(ar07w['lat'].values,ds['Z'],np.ma.masked_values(fld[0,:,:],0),30,cmap=cm.gist_ncar)
 plt.colorbar()
-plt.title('WOA temperature at AR07W section')
+plt.title('ECCO temperature at AR07W section')
 plt.show()